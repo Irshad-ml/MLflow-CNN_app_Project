59aa98b88eccd58ea110d36068ced733637e722e
Md Irshad <m3irshad3@gmail.com>
2022-05-15 21:13:53 +0530

    Stage 01 updated

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -31,10 +31,10 @@ def main(config_path, params_path):
     create_directories([local_dir])
 
     data_file = config["data"]["data_file"]
-    print(data_file)
+    print("data: ",data_file)
     #once the local directory created and file created then we create final path where we store the data
     data_file_path = os.path.join(local_dir,data_file)
-    print(data_file_path)
+    print("path of the file: ",data_file_path)
 
     #req.urlretrieve() function helps to download the data from the online source and store into the destination path and 
     #return filename and headers
@@ -43,15 +43,20 @@ def main(config_path, params_path):
         filename ,headers = req.urlretrieve(URL, data_file_path)
         logging.info(f"filename: {filename} created with info {headers}")
     else:
-        print(f"{data_file_path} is already present")
+        logging.info(f"{data_file_path} is already present")
     #params = read_yaml(params_path)
     #pass
 
     #Unzipped 
     unzipped_dir = config["data"]["unzip_data_dir"]
-    create_directories([unzipped_dir]) 
-    print("unzipped directory: ",unzipped_dir)
-    unzip_file(source = data_file_path, dest = unzipped_dir)
+    if not os.path.isdir(unzipped_dir):
+         create_directories([unzipped_dir]) 
+         print("unzipped directory: ",unzipped_dir)    
+         unzip_file(source = data_file_path, dest = unzipped_dir)
+    else:
+        logging.info(f"data is already extrated")
+        
+        
 
     #config value == {'data': {'source_url': 'https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip', 
     # 'local_dir': 'data', 
